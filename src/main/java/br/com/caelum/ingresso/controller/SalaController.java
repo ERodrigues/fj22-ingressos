e29296dd7be4f6bb15b20fdd773255659c226852
eder.rodrigues <ederpr@gmail.com>
2020-11-12 21:24:44 -0300

    adicao do metodo que ira criar uma nova sessao através do verbo POST do http

--- src/main/java/br/com/caelum/ingresso/controller/SalaController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SalaController.java
@@ -1,6 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
 import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.form.SalaForm;
 
@@ -21,8 +22,9 @@ import java.util.Optional;
 public class SalaController {
 
     @Autowired
-    private SalaDao salaDao;
-
+    private SalaDao salaDao;    
+    @Autowired 
+    private SessaoDao sessaoDao; 
 
     @GetMapping({"/admin/sala", "/admin/sala/{id}"})
     public ModelAndView form(@PathVariable("id") Optional<Integer> id, SalaForm salaForm) {
@@ -64,8 +66,9 @@ public class SalaController {
 
         Sala sala = salaDao.findOne(id);
 
-        ModelAndView view = new ModelAndView("sessao/lista");
+        ModelAndView view = new ModelAndView("sessao/lista");        
         view.addObject("sala", sala);
+        view.addObject("sessoes", sessaoDao.buscaSessoesDaSala(sala));
 
         return view;
     }
