b77b7434d655bcdcd48ca8c6c1be8a6e1f500dd9
eder.rodrigues <ederpr@gmail.com>
2020-11-19 20:21:26 -0300

    classe adaptada para testar a classe Ingresso

--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -10,8 +10,10 @@ import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Lugar;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.descontos.TipoDeIngresso;
 import junit.framework.Assert;
 
 public class DescontoTest {
@@ -19,23 +21,25 @@ public class DescontoTest {
 	Sala sala; 
 	Filme filme; 
 	Sessao sessao; 
+	Lugar lugar; 
 	
 	@Before
 	public void preparaSessaoDesconto() {
 		sala = new Sala("Eldorado Max", new BigDecimal("20.5"));
 		filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
 		sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		lugar = new Lugar("Fila H", 3);
 	}
 
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal() {
-		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.INTEIRO, lugar);
 		Assert.assertEquals(ingresso.getPreco(), new BigDecimal("32.50"));		
 	}
 	
 	@Test
 	public void deveConcederDescontoParaIngressoDeEstudante() {
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.ESTUDANTE, lugar);
 		BigDecimal valorComDesconto = sessao.getPreco().divide(new BigDecimal("2.0"));
 		Assert.assertEquals(ingresso.getPreco(), valorComDesconto);
 		
@@ -43,7 +47,7 @@ public class DescontoTest {
 	
 	@Test 
 	public void deveConcederDescontoParaIngressoDeFuncionariosDeBanco() {
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.BANCO, lugar);
 		BigDecimal valorComDesconto = sessao.getPreco().subtract(sessao.getPreco().multiply(new BigDecimal("0.3")));
 		Assert.assertEquals(ingresso.getPreco(), valorComDesconto.setScale(2, RoundingMode.HALF_UP));
 	}
