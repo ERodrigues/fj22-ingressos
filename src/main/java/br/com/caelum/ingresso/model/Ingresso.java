6b345f3a47b68b98ce95afd7ec8fd84e46b00c24
eder.rodrigues <ederpr@gmail.com>
2020-11-19 21:10:55 -0300

    Adicionado as anotacoes do Hibernate para persistir o ingresso no banco

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -3,16 +3,38 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 import br.com.caelum.ingresso.model.descontos.TipoDeIngresso;
 import br.com.caelum.ingresso.model.Sessao; 
 
+@Entity
 public class Ingresso {
-	private Sessao sessao; 
-	private BigDecimal preco; 
-	private Lugar lugar; 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; 
+	@ManyToOne
+	private Sessao sessao;
+	private BigDecimal preco;
+	@ManyToOne
+	private Lugar lugar;
+	@Enumerated(EnumType.STRING)
+	private TipoDeIngresso tipoDeIngresso; 
+	
+	public Ingresso(){
+		
+	}
 	
 	public Ingresso(Sessao sessao, TipoDeIngresso tipoDeIngresso, Lugar lugar) {
 		this.sessao = sessao; 
+		this.tipoDeIngresso = tipoDeIngresso; 
 		this.preco = tipoDeIngresso.aplicaDesconto(sessao.getPreco());
 		this.lugar = lugar; 
 	}
@@ -24,4 +46,40 @@ public class Ingresso {
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco; 
 	}
+
+	public TipoDeIngresso getTipoDeIngresso() {
+		return tipoDeIngresso;
+	}
+
+	public void setTipoDeIngresso(TipoDeIngresso tipoDeIngresso) {
+		this.tipoDeIngresso = tipoDeIngresso;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Sessao getSessao() {
+		return sessao;
+	}
+
+	public void setSessao(Sessao sessao) {
+		this.sessao = sessao;
+	}
+
+	public Lugar getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(Lugar lugar) {
+		this.lugar = lugar;
+	}
+	
+	
+	
+	
 }
