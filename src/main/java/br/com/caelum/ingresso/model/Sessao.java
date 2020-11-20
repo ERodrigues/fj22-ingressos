3630464a773a994dac1010c418ab382d01880958
eder.rodrigues <ederpr@gmail.com>
2020-11-19 21:12:50 -0300

    adicionado o mapa de ingressos da sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,14 +3,20 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import br.com.caelum.ingresso.model.Ingresso; 
 
 @Entity
 public class Sessao {
@@ -24,6 +30,8 @@ public class Sessao {
 	@ManyToOne
 	private Filme filme;
 	private BigDecimal preco; 
+	@OneToMany(mappedBy="sessao", fetch=FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
 	
 	public Sessao() {
 		
@@ -72,5 +80,17 @@ public class Sessao {
 	public Map<String, List<Lugar>> getMapaDeLugares(){
 		return sala.getMapaDeLugares(); 
 	}
+
+	public Set<Ingresso> getIngressos() {
+		return ingressos;
+	}
+
+	public void setIngressos(Set<Ingresso> ingressos) {
+		this.ingressos = ingressos;
+	}
 	
+	public boolean isDisponivel(Lugar lugarSelecionado) {
+		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+	}
+		
 }
